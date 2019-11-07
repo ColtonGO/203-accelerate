bf0c3faf6638299d85dcdd4fd7533676ada0a29c
Colton Organ <colton.organ@gmail.com>
2019-11-07 15:32:59 -0800

    added social icons menu and styled

--- wp-content/themes/accelerate-theme-child/archive-case_studies.php
+++ wp-content/themes/accelerate-theme-child/archive-case_studies.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * The template for displaying a single case study.
- *
+ * The template for displaying the case study archive.
  *
  * @package WordPress
  * @subpackage Accelerate Marketing
@@ -10,53 +9,40 @@
 
 get_header(); ?>
 
-	<div id="primary" class="site-content">
+	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
-			
-			<?php while ( have_posts() ) : the_post();
+        <?php while ( have_posts() ) : the_post();
 				$size = "full";
 				$services = get_field('services');
 				$client = get_field('client');
 				$link = get_field('site_link');
-				$image_1 = get_field('image_1');
-				$image_2 = get_field('image_2');
-				$image_3 = get_field('image_3');?>
+				$image_1 = get_field('image_1');?>
 
 			<article class="case-study">
 				<aside class="case-study-sidebar">
-				<h2><?php the_title(); ?></h2>
+                <a href="<?php the_permalink(); ?>">
+                    <h2><?php the_title(); ?></h2>
+                </a>
 				<h6 class="grey-font"><?php echo $services; ?></h6>
 				<h6>Client: <?php echo $client; ?></h6>
 
-				<?php the_content(); ?>
+				<?php the_excerpt(); ?>
 
 				<p class="read-more-link"><strong><a href="<?php echo $link; ?>">Visit Live Site ></a></strong></p>
 				</aside>
 
 				<div class="case-study-images">
+                    <a href="<?php the_permalink(); ?>">
 					<?php if($image_1) {
 						echo wp_get_attachment_image( $image_1, $size );
-					 } ?>
-
-					<?php if($image_2) {
-						echo wp_get_attachment_image( $image_2, $size );
-					 } ?>
-
-					<?php if($image_3) {
-						echo wp_get_attachment_image( $image_3, $size );
-					 } ?>
+                     } ?>
+                     </a>
 				</div>
 
 			</article>
-
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 
-
 	</div><!-- #primary -->
-
-	<nav id="navigation" class="container">
-		<div class="left"><a href="<?php echo site_url('/case-studies/') ?>">&larr; <span>Back to work</span></a></div>
-	</nav>
 
 <?php get_footer(); ?>
