290dd7bb9be368fff2e6c5cfa0eda86576369f03
Colton Organ <colton.organ@gmail.com>
2019-11-09 23:14:56 -0800

    added ACF for about call to action and finished the hero content

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -15,20 +15,29 @@ get_header(); ?>
 		$size = 'full';
 		$hero_image = get_field('hero_image');
 		$hero_text = get_field('hero_text');
+		$call_to_action = get_field('call_to_action');
+		$about_button = get_field('about_button');
 		?>
+		<h2 class="hero-text">
+			<?php if($hero_text) {
+				echo $hero_text;
+			} ?>
+		</h2>
+
 		<?php if($hero_image) {
 			echo wp_get_attachment_image( $hero_image, $size );
-			} ?>
+		} ?>
 	</section>
 
 	<div id="primary" class="site-content sidebar">
 		<div class="main-content" role="main">
+			<div class="about-content">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
 			<?php endwhile; // end of the loop. ?>
+			</div>
 		</div><!-- .main-content -->
-
 
 	</div><!-- #primary -->
 
@@ -59,9 +68,22 @@ get_header(); ?>
 				<?php endwhile; // end of the loop. ?>
 			</ul>
 
+			<section class="about-cta">
+				<h2 class="call-to-action">
+					<?php if($call_to_action) {
+						echo $call_to_action;
+					} ?>
+				</h2>
+				<button class="button">
+					<?php if($about_button) {
+						echo $about_button;
+					} ?>
+				</button>
+			</section>	
+
 		</div><!-- .main-content -->
 
 
-				</section>
+	</section>
 
 <?php get_footer(); ?>
