0688d4c331520de54d28f3676482080863ef2ab4
Colton Organ <colton.organ@gmail.com>
2019-11-14 13:56:36 -0800

    added 404 page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying 404 page not found
+ *
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+			<section class="not-found">
+				<div class="whoops-img">
+					<img src="wp-content/themes/accelerate-theme-child/img/not-found.png">
+				</div>
+				<div class="whoops-content">
+					<h1>Whoops, Took a Wrong Turn...</h1>
+					<p>Sorry, this page no longer exists, never existed or has been moved. We feel like complete jerks for totally misleading you.<br>Feel free to take a look around our <a href='/blog'>blog</a> or some of our featured <a href="/work/">work</a>.</p>
+				</div>
+			</section>
+		</div><!-- .main-content -->
+
+
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
